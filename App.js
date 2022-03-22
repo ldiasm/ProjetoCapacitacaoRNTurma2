8298abfa669fca617812a6b7eeb85ee000d6ae2c
lucasdpmerces <lucasmerces@gmail.com>
2022-03-22 18:02:44 -0300

    Commit inicial, turma 02

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Login from './src/screens/Login';
+
+const App = () => {
+  return (
+    <Login/>
+  );
+};
+
+export default App;
---
+++ src/screens/Login.js
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      background: require('../assets/images/loginBackground.jpg'),
+      title: 'Beijo no neném' 
+    }
+  }
+
+  backgroundChange = () => {
+    this.setState({
+      background: require('../assets/images/ballonImage.jpg'),
+      title: 'Sentada com balões'
+    })
+    console.log("Pressionou!", this.state.title)
+  }
+
+  render() {
+    return (
+      <View style={styles.mainView}>
+        <ImageBackground
+          source={this.state.background}
+          resizeMode="cover"
+          style={styles.imageBackground}
+        >
+          <View style={styles.titleView}>
+            <Text style={styles.titleText}>{`${this.state.title}`}</Text>
+          </View>
+          <View style={styles.textInputView}>
+            <TextInput
+              style={styles.textInput}
+              keyboardType="email-address"
+              placeholder={'EMAIL'}
+            />
+            <TextInput style={styles.textInput} placeholder={'PASSWORD'} />
+            <TouchableOpacity style={styles.button} onPress={() => this.backgroundChange()}>
+              <Text style={styles.buttonText}>{`LOGIN`}</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mainView: {
+    height: '100%',
+    backgroundColor: 'black',
+  },
+  imageBackground: {
+    height: '100%',
+    padding: 32,
+  },
+  titleView: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: 36,
+    color: 'white',
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(0,0,0,0.4)'
+  },
+  textInputView: {
+    flex: 2,
+  },
+  textInput: {
+    borderColor: '#777777',
+    borderWidth: 3,
+    borderRadius: 16,
+    marginVertical: 8,
+    backgroundColor: 'rgba(255,255,255,0.4)',
+  },
+  button: {
+    backgroundColor: '#7711AA',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '600'
+  }
+});
